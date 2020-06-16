caa6192b12e130018826a524272ee2f39d119153
Arsi <66896926+LeArsiVU@users.noreply.github.com>
2020-06-15 20:01:18 -0600

    comentarios
    comentarios y excluir lo de la puntualidad Z en los clientes de contado.

--- crear_tablas_de_peticion.sql
+++ crear_tablas_de_peticion.sql
@@ -1,15 +1,16 @@
-CREATE TABLE digitales_cuenta_activa_tem AS 
+CREATE TABLE digitales_cuenta_activa_tem AS -- En esta tabla van a estar todos los correos que ha dado de alta el cliente
 select idu_ClienteCodigoMaestro as idu_ClienteCodigo, des_CorreoElectronico, fec_FechaAltaCorreo
 FROM mae_correosdatamart as t1
 JOIN Rs_CategoriasEdoResultado_Rova as t2 ON
 t1.idu_ClienteCodigoMaestro = t2.clientecodigo and 
 t2.Segmento IN ('Activo Sin Vencido','Vencido1','Vencido2','Vencido3','Vencido +4')
-where LOCATE("Z",idu_PuntualidadCodigo)=0 and
-flag_registroactivo = 1 and
-idu_ClienteCodigoMaestro <> 90001 and 
-idu_ClienteCodigoMaestro  NOT BETWEEN 90000000 and 99999999;
+where LOCATE("Z",idu_PuntualidadCodigo)=0 and -- Excluye la puntualidad Z
+flag_registroactivo = 1 and -- Cuenta activada
+idu_ClienteCodigoMaestro <> 90001 and -- Excluye clientes de contado
+idu_ClienteCodigoMaestro  NOT BETWEEN 90000000 and 99999999; -- Excluye empleados
 
-CREATE TABLE digitales_cuenta_activa AS (select t1.idu_ClienteCodigo, t1.des_CorreoElectronico
+CREATE TABLE digitales_cuenta_activa AS -- En esta tabla ya está sólo el correo que se ha dado de alta más recientemente
+(select t1.idu_ClienteCodigo, t1.des_CorreoElectronico
  FROM digitales_cuenta_activa_tem as t1
  JOIN  digitales_cuenta_activa_tem
  on t1.idu_ClienteCodigo=digitales_cuenta_activa_tem.idu_ClienteCodigo and 
@@ -19,18 +20,19 @@ CREATE TABLE digitales_cuenta_activa AS (select t1.idu_ClienteCodigo, t1.des_Cor
 DROP table digitales_cuenta_activa_tem;
 
 
-CREATE TABLE digitales_cuenta_sin_activar_tem AS 
+CREATE TABLE digitales_cuenta_sin_activar_tem AS -- En esta tabla van a estar todos los correos que ha dado de alta el cliente
 select idu_ClienteCodigoMaestro  as idu_ClienteCodigo, des_CorreoElectronico, fec_FechaAltaCorreo
 FROM mae_correosdatamart as t1
 JOIN Rs_CategoriasEdoResultado_Rova as t2 ON
 t1.idu_ClienteCodigoMaestro = t2.clientecodigo and 
 t2.Segmento IN ('Activo Sin Vencido','Vencido1','Vencido2','Vencido3','Vencido +4')
-where LOCATE("Z",idu_PuntualidadCodigo)=0 and
-flag_registroactivo <> 1 and
-idu_ClienteCodigoMaestro <> 90001 and 
-idu_ClienteCodigoMaestro  NOT BETWEEN 90000000 and 99999999;
+where LOCATE("Z",idu_PuntualidadCodigo)=0 and -- Excluye la puntualidad Z
+flag_registroactivo <> 1 and -- Cuenta sin activar
+idu_ClienteCodigoMaestro <> 90001 and -- Excluye clientes de contado
+idu_ClienteCodigoMaestro  NOT BETWEEN 90000000 and 99999999; -- Excluye empleados
 
-CREATE TABLE digitales_cuenta_sin_activar AS (select t1.idu_ClienteCodigo, t1.des_CorreoElectronico
+CREATE TABLE digitales_cuenta_sin_activar AS -- En esta tabla ya está sólo el correo que se ha dado de alta más recientemente
+(select t1.idu_ClienteCodigo, t1.des_CorreoElectronico
  FROM digitales_cuenta_sin_activar_tem as t1
  JOIN  digitales_cuenta_sin_activar_tem
  on t1.idu_ClienteCodigo=digitales_cuenta_sin_activar_tem.idu_ClienteCodigo and 
@@ -43,15 +45,13 @@ CREATE TABLE digitales_cuenta_sin_activar AS (select t1.idu_ClienteCodigo, t1.de
 create  TABLE Clientes_Contado_abonar_terceros_activo AS
 select idu_ClienteCodigoMaestro  as idu_ClienteCodigo, des_CorreoElectronico
 FROM mae_correosdatamart 
-where LOCATE("Z",idu_PuntualidadCodigo)=0 and
-flag_registroactivo = 1 and
-idu_ClienteCodigoMaestro = 90001 and 
-YEAR(fec_FechaAltaCorreo)>=2017;
+where flag_registroactivo = 1 and -- Cuenta activada
+idu_ClienteCodigoMaestro = 90001 and -- Cliente de contado
+YEAR(fec_FechaAltaCorreo)>=2017; -- Fecha de captura del correo de 2017 en adelante
 
 create  TABLE Clientes_Contado_abonar_terceros_sin_activar AS
 select idu_ClienteCodigoMaestro  as idu_ClienteCodigo, des_CorreoElectronico
 FROM mae_correosdatamart 
-where LOCATE("Z",idu_PuntualidadCodigo)=0 and
-flag_registroactivo <> 1 and
-idu_ClienteCodigoMaestro = 90001 and 
-YEAR(fec_FechaAltaCorreo)>=2017;
+where flag_registroactivo <> 1 and -- Cuenta sin activar
+idu_ClienteCodigoMaestro = 90001 and -- Cliente de contado
+YEAR(fec_FechaAltaCorreo)>=2017; -- Fecha de captura del correo de 2017 en adelante
