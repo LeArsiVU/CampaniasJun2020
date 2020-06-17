ec98af46678640168df99d3fb5b3c6852cb1e625
Arsi <66896926+LeArsiVU@users.noreply.github.com>
2020-06-17 10:26:23 -0600

    Update crear_tablas_de_peticion.sql

--- crear_tablas_de_peticion.sql
+++ crear_tablas_de_peticion.sql
@@ -1,13 +1,13 @@
-CREATE TABLE digitales_cuenta_activa_tem AS 
-select CAST(FLOOR(idu_ClienteCodigoMaestro/10) as SIGNED) as idu_ClienteCodigo, des_CorreoElectronico, fec_FechaAltaCorreo
+CREATE TABLE digitales_cuenta_activa_tem AS -- En esta tabla van a estar todos los correos que ha dado de alta el cliente
+select idu_ClienteCodigoMaestro as idu_ClienteCodigo, des_CorreoElectronico, fec_FechaAltaCorreo
 FROM mae_correosdatamart as t1
 JOIN Rs_CategoriasEdoResultado_Rova as t2 ON
-t1.idu_ClienteCodigoMaestro = t2.clientecodigo and 
+CAST(FLOOR(t1.idu_ClienteCodigoMaestro/10) as SIGNED)= t2.clientecodigo and 
 t2.Segmento IN ('Activo Sin Vencido','Vencido1','Vencido2','Vencido3','Vencido +4')
-where LOCATE("Z",idu_PuntualidadCodigo)=0 and
-flag_registroactivo = 1 and
-idu_ClienteCodigoMaestro <> 90001 and 
-CAST(FLOOR(idu_ClienteCodigoMaestro/10) as SIGNED)  NOT BETWEEN 90000000 and 99999999;
+where LOCATE("Z",idu_PuntualidadCodigo)=0 and -- Excluye la puntualidad Z
+flag_registroactivo = 1 and -- Cuenta activada
+idu_ClienteCodigoMaestro <> 90001 and -- Excluye clientes de contado
+idu_ClienteCodigoMaestro  NOT BETWEEN 90000000 and 99999999; -- Excluye empleados
 
 CREATE TABLE digitales_cuenta_activa AS -- En esta tabla ya está sólo el correo que se ha dado de alta más recientemente
 (select t1.idu_ClienteCodigo, t1.des_CorreoElectronico
@@ -19,16 +19,17 @@ CREATE TABLE digitales_cuenta_activa AS -- En esta tabla ya está sólo el corre
  
 DROP table digitales_cuenta_activa_tem;
 
-CREATE TABLE digitales_cuenta_sin_activar_tem AS 
-select CAST(FLOOR(idu_ClienteCodigoMaestro/10) as SIGNED)  as idu_ClienteCodigo, des_CorreoElectronico, fec_FechaAltaCorreo
+
+CREATE TABLE digitales_cuenta_sin_activar_tem AS -- En esta tabla van a estar todos los correos que ha dado de alta el cliente
+select idu_ClienteCodigoMaestro  as idu_ClienteCodigo, des_CorreoElectronico, fec_FechaAltaCorreo
 FROM mae_correosdatamart as t1
 JOIN Rs_CategoriasEdoResultado_Rova as t2 ON
-t1.idu_ClienteCodigoMaestro = t2.clientecodigo and 
+CAST(FLOOR(t1.idu_ClienteCodigoMaestro/10) as SIGNED)= t2.clientecodigo and 
 t2.Segmento IN ('Activo Sin Vencido','Vencido1','Vencido2','Vencido3','Vencido +4')
-where LOCATE("Z",idu_PuntualidadCodigo)=0 and
-flag_registroactivo <> 1 and
-idu_ClienteCodigoMaestro <> 90001 and 
-CAST(FLOOR(idu_ClienteCodigoMaestro/10) as SIGNED)  NOT BETWEEN 90000000 and 99999999;
+where LOCATE("Z",idu_PuntualidadCodigo)=0 and -- Excluye la puntualidad Z
+flag_registroactivo <> 1 and -- Cuenta sin activar
+idu_ClienteCodigoMaestro <> 90001 and -- Excluye clientes de contado
+idu_ClienteCodigoMaestro  NOT BETWEEN 90000000 and 99999999; -- Excluye empleados
 
 CREATE TABLE digitales_cuenta_sin_activar AS -- En esta tabla ya está sólo el correo que se ha dado de alta más recientemente
 (select t1.idu_ClienteCodigo, t1.des_CorreoElectronico
@@ -39,7 +40,6 @@ CREATE TABLE digitales_cuenta_sin_activar AS -- En esta tabla ya está sólo el 
  group by t1.idu_ClienteCodigo, t1.des_CorreoElectronico);
  
  DROP table digitales_cuenta_sin_activar_tem;
-
 
 create  TABLE Clientes_Contado_abonar_terceros_activo AS
 select idu_ClienteCodigoMaestro  as idu_ClienteCodigo, des_CorreoElectronico
